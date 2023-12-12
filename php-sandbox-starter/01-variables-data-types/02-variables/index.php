f66286a2e236f72977ad4b390490fe7f6cb5dda3
Husson <axel.husson@smile.fr>
2023-12-12 11:51:03 +0100

    docs(01-variables-data-types): complete section on PHP variables

--- php-sandbox-starter/01-variables-data-types/02-variables/index.php
+++ php-sandbox-starter/01-variables-data-types/02-variables/index.php
@@ -15,6 +15,12 @@ CONVENTIONS:
 - Pascal Case: $ServerName
 - lowercase: $servername
 */
+
+// Creating variables: 
+
+$heading = "Learn PHP From Scratch";
+$title = "Welcome to the course";
+$description = "In this course, you will learn the fundamentals of the PHP language";
 ?>
 
 <!DOCTYPE html>
@@ -30,15 +36,14 @@ CONVENTIONS:
 <body class="bg-gray-100">
     <header class="bg-blue-500 text-white p-4">
         <div class="container mx-auto">
-            <h1 class="text-3xl font-semibold">Learn PHP From Scratch</h1>
+            <h1 class="text-3xl font-semibold"><?= $heading ?></h1>
         </div>
     </header>
     <div class="container mx-auto p-4 mt-4">
         <div class="bg-white rounded-lg shadow-md p-6">
-            <h2 class="text-2xl font-semibold mb-4">Welcome To The Course</h2>
-            <p>In this course, you will learn the fundamentals of the PHP language</p>
+            <h2 class="text-2xl font-semibold mb-4"><?= $title ?></h2>
+            <p><?= $description ?></p>
         </div>
     </div>
 </body>
-
 </html>
